7a2757525236b1e68a4b6d9659cc842a625e6238
yuuitiroo <game22403047@gmail.com>
2024-07-11 10:43:11 +0900

    l

---
+++ testing/test.js
@@ -0,0 +1,11 @@
+function flip(element) {
+    if (element.innerHTML == "Hello World!!") {
+        element.innerHTML = 'Goodbye World TwT';
+    }
+    else{
+        element.innerHTML= "Hello World!!";
+    }
+}
+function disappear() {
+    alert("Guess I'm dead");
+}
