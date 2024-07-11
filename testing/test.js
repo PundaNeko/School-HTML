3a9f3c1a4f735e1242d908490ceabc5c2d33920f
yuuitiroo <game22403047@gmail.com>
2024-07-11 11:04:24 +0900

    a

--- testing/test.js
+++ testing/test.js
@@ -1,10 +1,20 @@
 function flip(element) {
-    if (element.innerHTML == "Hello World!!") {
-        element.innerHTML = 'Goodbye World TwT';
-    }
-    else{
-        element.innerHTML= "Hello World!!";
-    }
+    currentText.classList.add('hidden');
+    setTimeout(() => {
+        if (element.innerHTML == "Hello World!!") {
+            element.innerHTML = 'Goodbye World TwT';
+        }
+        else{
+            element.innerHTML= "Hello World!!";
+        }
+        currentText.classList.remove('hidden');
+    }, 500); // Match the duration in the CSS transition property
+}
+function changeText(element) {
+    // Get the current text element
+    const currentText = element;
+    // Set the text to fade out
+    // Wait for the fade-out transition to finish
 }
 function disappear() {
     alert("Guess I'm dead");
